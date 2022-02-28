a9bda44b4f55f62240b6835eb1ab7bd3764f8766
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-03-01 02:09:00 +0600

    layout done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 document.getElementById('mobile-trapper').style.color = 'green';
+document.getElementById('main-body').style.backgroundColor = '#c6f3e4c4';
+
+const loadPhone = () => {
+    const inputSearch = document.getElementById('input-search').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputSearch}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showPhoneDetail(data.data))
+}
+
+const showPhoneDetail = (infos) => {
+    // console.log(infos);
+    const phoneContainer = document.getElementById('phone-container');
+    for (const phone of infos) {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = ` 
+            <div class="card h-100">
+                <div class="text-center">
+                    <img src="${phone.image}" alt="">
+                </div>
+                <h2 class="text-center">Name: ${phone.phone_name}</h2>
+                <h2 class="text-center">Brand: ${phone.brand}</h2>
+                <button onclick="" type="button" class="btn btn-info">Explore</button>
+            </div>
+        `;
+        phoneContainer.appendChild(div);
+    }
+}
