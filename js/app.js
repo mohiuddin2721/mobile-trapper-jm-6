c6dbdcebba5f7877cd22d2a5413bd3bf80796df1
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-03-01 13:32:09 +0600

    first erroe massage done

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 document.getElementById('mobile-trapper').style.color = 'green';
 document.getElementById('main-body').style.backgroundColor = '#c6f3e4c4';
+document.getElementById('error-message').style.display = 'none';
 
 const loadPhone = () => {
     const inputSearch = document.getElementById('input-search').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${inputSearch}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => showPhoneDetail(data.data))
+    inputSearch.innerText = '';
+    document.getElementById('error-message').style.display = 'none';
+    if(inputSearch == '') {
+        // please wright something to display
+        document.getElementById('error-message').style.display = 'block';
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputSearch}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => showPhoneDetail(data.data))
+    } 
+}
+
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
 }
 
 const showPhoneDetail = (infos) => {
     // console.log(infos);
     const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
     for (const phone of infos) {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -21,7 +35,7 @@ const showPhoneDetail = (infos) => {
                     <img src="${phone.image}" alt="">
                 </div>
                 <h2 class="text-center">Name: ${phone.phone_name}</h2>
-                <h2 class="text-center">Brand: ${phone.brand}</h2>
+                <h3 class="text-center">Brand: ${phone.brand}</h3>
                 <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-info">Explore</button>
             </div>
         `;
@@ -38,18 +52,29 @@ const loadPhoneDetail = (details) => {
 }
 
 const displayPhoneDetail = (explore) => {
-    const displayContainer = document.getElementById('display-container');
+    console.log(explore.mainFeatures);
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
+    div.classList.add("card")
     div.innerHTML = `
-    <div class="card">
-        <div>
-            <img src="${explore.image}" alt="">
-        </div>
-        <h2>${explore.name}</h2>
-        <h3>${explore}</h3>
-        <h4>${explore}</h4>
-        <h4>${explore}</h4>
+    <div class="text-center">
+        <img src="${explore.image}" class="card-img-top w-50" alt="...">
+    </div>
+    <div class="card-body text-center">
+      <h3 class="card-title">${explore.name}</h3>
+      <p class="card-text text-danger">Release Date: ${explore.releaseDate}</p>
+      <div>
+        <h3 class="text-center">Main Features</h3>
+        <p class="card-text text-info">ChipSet: ${explore.mainFeatures.chipSet}</p>
+        <p class="card-text text-info">DisplaySize: ${explore.mainFeatures.displaySize}</p>
+        <p class="card-text text-info">Memory: ${explore.mainFeatures.memory}</p>
+      </div>
+      <div>
+        <h3 class="text-center">Others</h3>
+        <p class="card-text text-info">${explore.mainFeatures.sensors}</p>
+      </div>
     </div>
     `;
-    displayContainer.appendChild(div);
+    phoneDetails.appendChild(div);
 }
